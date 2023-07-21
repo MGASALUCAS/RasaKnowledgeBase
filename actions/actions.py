43add42a1ccd7965aefdf7b761044b657b6a3cad
MGASALUCAS <mgasa.loucat1@gmail.com>
2023-07-21 10:35:51 +0300

    Updated rasa action

--- actions/actions.py
+++ actions/actions.py
@@ -39,6 +39,30 @@
 
 
 
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+import os
+
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_hello_world"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Welcome to the our digital assistance! Start by just saying Hello. "
+                                      "Let's get started!"
+                                      )
+
+        return []
+
+
+
 from rasa_sdk import Action
 import openai
 import os
